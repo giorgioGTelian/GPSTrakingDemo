c754e565645c0280f9bf04ea07e2cc5c0f1bde93
giorgio <giorgiobiologist@gmail.com>
2022-11-02 23:12:33 +0100

    new project

--- app/src/main/java/com/example/gpstrakingdemo/MainActivity.java
+++ app/src/main/java/com/example/gpstrakingdemo/MainActivity.java
@@ -3,16 +3,21 @@ package com.example.gpstrakingdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Switch;
 import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tv_lat, tv_lon, tv_altitude, tv_accuracy, tv_speed, tv_sensor, tv_updates, tv_address;
 
     Switch sw_locationsupdates, sw_gps;
+
+    //location request is a config file for all setting related to FusedLocationProviderClient
+    LocationRequest locationRequest;
 
     //Google's API for location services. The majority of the app function using this class
     FusedLocationProviderClient fusedLocationProviderClient;
@@ -31,5 +36,30 @@ public class MainActivity extends AppCompatActivity {
         tv_updates = findViewById(R.id.tv_updates);
         sw_gps = findViewById(R.id.sw_gps);
         sw_locationsupdates = findViewById(R.id.sw_locationsupdates);
-    }
+
+        //set all properties of locationRequest
+        locationRequest = new LocationRequest();
+
+        locationRequest.setInterval(30000);
+
+        locationRequest.setFastestInterval(5000);
+
+        locationRequest.setPriority(locationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        sw_gps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (sw_gps.isChecked()) {
+                    //most accurate - use GPS
+                    locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                    tv_sensor.setText("Using GPS sensors");
+                }
+                else {
+                    locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+                    tv_sensor.setText("using Towers + Wifi");
+                }
+            }
+        });
+    } //end onCreate method
+
 }
