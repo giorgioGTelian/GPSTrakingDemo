d1aea79d707760a1a50feb40989aa32e1b448760
giorgioGTelian <giorgiobiologist@gmail.com>
2022-11-06 00:09:15 +0100

    now everytime i open the app i have a location

--- app/src/main/java/com/example/gpstrakingdemo/MainActivity.java
+++ app/src/main/java/com/example/gpstrakingdemo/MainActivity.java
@@ -1,20 +1,37 @@
 package com.example.gpstrakingdemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int DEFAULT_UPDATE_INTERVAL = 30;
+    private static final int FAST_UPDATE_INTERVAL = 5;
+    private static final int PERMISSION_FINE_LOCATION = 99;
+
+    //references to the UI elements.
     TextView tv_lat, tv_lon, tv_altitude, tv_accuracy, tv_speed, tv_sensor, tv_updates, tv_address;
 
     Switch sw_locationsupdates, sw_gps;
+
+    // variable to remember if we are tracking location or not.
+    boolean updateOn = false;
 
     //location request is a config file for all setting related to FusedLocationProviderClient
     LocationRequest locationRequest;
@@ -60,6 +77,69 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        updateGPS();
+
+
     } //end onCreate method
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case PERMISSION_FINE_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    updateGPS();
+                }
+                else {
+                    Toast.makeText(this, "This app requires permission in order to work", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                break;
+        }
+    }
+
+    private void updateGPS() {
+        //get permissions from the user to track GPS
+        //get the current location from the fused client
+        //update te UI - i.e. set all properties in their associated text view items.
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // user provide permission?!?
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    // we got permissions. Put the values of location. XXX into the UI components.
+                    updateUIValues(location);
+                }
+            });
+        }
+        else {
+            //ask for permissions!
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_FINE_LOCATION);
+            }
+        }
+    }
+    private void updateUIValues(Location location) {
+        //update all of the text view objects with a new location.
+        tv_lat.setText(String.valueOf(location.getLatitude()));
+        tv_lon.setText(String.valueOf(location.getLongitude()));
+        tv_accuracy.setText(String.valueOf(location.getAltitude()));
+
+        if (location.hasAltitude()) {
+            tv_altitude.setText(String.valueOf(location.getAltitude()));
+        }
+        else {
+            tv_altitude.setText("Not available");
+        }
+        if (location.hasSpeed()) {
+            tv_speed.setText(String.valueOf(location.getSpeed()));
+        }
+        else {
+            tv_speed.setText("Not available");
+        }
+    }
 }
